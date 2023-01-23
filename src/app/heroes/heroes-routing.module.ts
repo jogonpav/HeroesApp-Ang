134d2a59c88d05521ac7be314241686f8f992e63
jogonpav <josepabonvera@gmail.com>
2023-01-23 16:15:21 -0500

    "Added child route for heores component"

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './pages/list/list.component';
 import { AddingComponent } from './pages/adding/adding.component';
 import { SearchingComponent } from './pages/searching/searching.component';
 import { HeoreComponent } from './pages/heore/heore.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path:'',
+    component: HomeComponent,
     children:[
      {
       path: 'list',
@@ -40,7 +41,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule.forChild(routes)
   ],
   exports:[
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AddingComponent } from './pages/adding/adding.component';
 import { SearchingComponent } from './pages/searching/searching.component';
 import { HeoreComponent } from './pages/heore/heore.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+
 
 
 
