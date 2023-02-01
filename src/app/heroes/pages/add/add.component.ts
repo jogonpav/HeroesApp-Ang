008780a460c3dfb381afd8072f4174cfaaf37305
jogonpav <josepabonvera@gmail.com>
2023-01-31 22:47:12 -0500

    Inserting add form

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add',
-  templateUrl: './add.component.html'
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
 })
 export class AddComponent {
 
