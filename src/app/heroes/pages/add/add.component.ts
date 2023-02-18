f70fbb2ee84fe4049e419cef2d3fcae57c29ea88
jogonpav <josepabonvera@gmail.com>
2023-02-17 21:31:30 -0500

    Added component Heroe Add

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+
+  publisher = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+    
+
+  
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,8 +9,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 
@@ -28,6 +30,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
