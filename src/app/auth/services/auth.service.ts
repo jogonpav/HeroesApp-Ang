142ccf5f8b0ff3ec529bcde9eb3ebd53e0137c55
jogonpav <josepabonvera@gmail.com>
2023-02-22 22:16:28 -0500

    "Added verification Auth"

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { Auth } from '../Interfaces/auth.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined; // Like this property is privated, is better to call a getter method
+
+  get auth(): Auth {
+    return { ...this._auth! }
+  }
+
+  constructor( private http: HttpClient) { }
+
+  login(): Observable<Auth>{
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+              .pipe(
+                tap( auth => this._auth = auth)
+              );
+  }
+}
