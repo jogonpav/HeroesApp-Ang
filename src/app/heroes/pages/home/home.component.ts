ee08fb794f438c758d46dce3bb928d6b7fa1a683
jogonpav <josepabonvera@gmail.com>
2023-02-22 22:25:15 -0500

    "Added loggin method"

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/Interfaces/auth.interface';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +13,19 @@ import { Component } from '@angular/core';
   `]
 })
 export class HomeComponent {
+
+  get auth(){
+    return this.authService.auth;
+  }
+
+  constructor( 
+              private router: Router,
+              private authService: AuthService
+              ){}
+
+ 
+  logout(){
+    this.router.navigate(['./auth'])
+  }
 
 }
